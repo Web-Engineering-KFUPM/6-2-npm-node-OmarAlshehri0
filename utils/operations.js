3a010d53060494d4953a8b3d63096d1cefdecf4e
omaralshehri <Alshehri2292@gmail.com>
2026-04-01 08:30:45 +0300

    vreate and complete operations.js

---
+++ utils/operations.js
@@ -0,0 +1,19 @@
+export function add(numbers) {
+  return numbers.reduce((sum,num) =>sum +num,0);
+}
+
+export function subtract(numbers) {
+  return numbers.slice(1).reduce((result,num) =>result -num,numbers[0]);
+}
+
+export function multiply(numbers) {
+  return numbers.reduce((result,num) =>result *num,1);
+}
+
+export function divide(numbers) {
+
+  if (numbers.slice(1).includes(0)) {
+    return "Cannot divide by zero";
+  }
+  return numbers.slice(1).reduce((result,num) =>result /num,numbers[0]);
+}
